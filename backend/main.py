4df43bf0ddb842d1a8d386a2eb96ca52c516e7ac
KUR4M4 <52566613+zp33dy@users.noreply.github.com>
2023-05-22 14:41:03 +0200

    Create main.py

---
+++ backend/main.py
@@ -0,0 +1,39 @@
+import asyncio
+from typing import *
+from datetime import datetime
+import tornado
+from tornado.websocket import WebSocketHandler
+
+class EchoWebSocket(WebSocketHandler):
+
+    def to_json(self, data, action: str, status: int = 200):
+        return {
+            "status": status,
+            "data": data,
+            "action": action,
+            "timestamp": datetime.now().timestamp(),
+            "sender": "server"
+        }
+    
+    def open(self):
+        print("WebSocket opened")
+
+    def on_message(self, message):
+        self.write_message(self.to_json(message, "echo", 200))
+
+    def on_close(self):
+        print("WebSocket closed")
+
+
+def make_app():
+    return tornado.web.Application([
+        (r"/", EchoWebSocket),
+    ])
+
+async def main():
+    app = make_app()
+    app.listen(8888)
+    await asyncio.Event().wait()
+
+if __name__ == "__main__":
+    asyncio.run(main())
