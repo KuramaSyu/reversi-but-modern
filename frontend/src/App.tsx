3ae5df908a4530f3d58b5a8d0ee6616085642154
KUR4M4 <52566613+zp33dy@users.noreply.github.com>
2023-05-24 08:01:52 +0200

    added dark theme

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,64 +1,102 @@
 import React, { useState, useEffect } from 'react';
-import './App.css';
+interface TitleBarProps {
+  title: string;
+  darkTheme: boolean;
+  onDarkThemeToggle: () => void;
+}
 
-const colors: string[] = ["emerald-500", "red-800"];
-let currentColorIndex = 0;
+let themes = {
+  light: {
+    titlebar_bg: 'bg-green-200',
+    bg: 'bg-green-100',
+    text: 'text-green-800',
+    buttons: 'bg-green-500 text-white',
+  },
+  dark: {
+    titlebar_bg: 'bg-slate-700',
+    bg: 'bg-slate-800',
+    text: 'text-white',
+    buttons: 'bg-slate-500 text-white',
+  },
+};
 
-function App() {
-  const [message, setMessage] = useState('');
-  const [response, setResponse] = useState('');
-  const [themeColor, setThemeColor] = useState(colors[currentColorIndex]);
-  
-  useEffect(() => {
-    // Connect to WebSocket
-    const ws = new WebSocket('ws://localhost:8888/');
-    // Handle received messages
-    ws.onmessage = (event) => {
-      const receivedMessage = event.data;
-      
-      // Update response field
-      setResponse(receivedMessage);
-      
-      // Cycle to the next color
-      currentColorIndex = (currentColorIndex + 1) % colors.length;
-      setThemeColor(colors[currentColorIndex]);
-    };
-
-    // Clean up the WebSocket connection on unmount
-    return () => {
-      ws.close();
-    };
-  });
-
-  const sendMessage = () => {
-    // Send message through the WebSocket
-    // Replace 'socket.send()' with your own logic
-    // For example: socket.send(message);
-    ws.send(message);
-    // Clear input field
-    setMessage('');
-  };
+/// TitleBar component
+const TitleBar: React.FC<{ title: string; darkTheme: boolean; onDarkThemeToggle: () => void }> = ({
+  title,
+  darkTheme,
+  onDarkThemeToggle,
+}) => {
+  const theme = darkTheme ? themes.dark : themes.light;
 
   return (
-    <div className={`bg-${themeColor} h-screen flex flex-col justify-center items-center`}>
+    <div className={`title-bar ${theme.titlebar_bg}`}>
+      <h1 className={`text-xl ${theme.text}`}>{title}</h1>
+      <button
+        className={`px-4 py-2 rounded ${theme.buttons}`}
+        onClick={onDarkThemeToggle}
+      >
+        {darkTheme ? 'Light Theme' : 'Dark Theme'}
+      </button>
+    </div>
+  );
+};
+
+// WebSocketDemo component
+const WebSocketDemo: React.FC = () => {
+  const [messages, setMessages] = useState<string[]>([]);
+  const [inputValue, setInputValue] = useState('');
+  const [socket, setSocket] = useState<WebSocket | null>(null);
+  const [darkTheme, setDarkTheme] = useState(false);
+
+  useEffect(() => {
+    const newSocket = new WebSocket('ws://localhost:8888/chat');
+    setSocket(newSocket);
+
+    newSocket.onmessage = (event) => {
+      const message = event.data;
+      setMessages((prevMessages) => [...prevMessages, message]);
+    };
+
+    return () => {
+      newSocket.close();
+    };
+  }, []);
+
+  const sendMessage = () => {
+    if (socket) {
+        socket.send(inputValue);
+        setInputValue('');
+    }
+  };
+
+  const toggleDarkTheme = () => {
+    setDarkTheme((prevDarkTheme) => !prevDarkTheme);
+  };
+
+  const theme = darkTheme ? themes.dark : themes.light;
+
+  return (
+    <div className={`app ${theme.bg}`}>
+      <TitleBar title="WebSocket Demo" darkTheme={darkTheme} onDarkThemeToggle={toggleDarkTheme} />
+      <div className={`message-list ${theme.text}`}>
+        {messages.map((message, index) => (
+          <p key={index}>{message}</p>
+        ))}
+      </div>
       <input
         type="text"
-        value={message}
-        onChange={(e) => setMessage(e.target.value)}
-        className="bg-white text-black rounded p-2"
-        placeholder="Write a message"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        className={`border rounded px-2 py-1 mt-2 ${theme.text}`}
       />
       <button
         onClick={sendMessage}
-        className="bg-white text-black rounded p-2 mt-2"
+        className={`px-4 py-2 rounded mt-2 ${theme.buttons}`}
       >
         Send
       </button>
-      <div className="text-white mt-4">
-        Response: {response}
-      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default WebSocketDemo;
