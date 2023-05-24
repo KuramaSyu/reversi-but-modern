fed75e0354eab2354bd9b4be73d405e8604d1148
KUR4M4 <52566613+zp33dy@users.noreply.github.com>
2023-05-24 15:20:31 +0200

    added themes

---
+++ frontend/src/themes.ts
@@ -0,0 +1,18 @@
+export const themes = {
+    light: {
+      titlebar_bg: 'bg-green-400',
+      bg: 'bg-green-100',
+      component: 'bg-green-200 hover:bg-green-300 hover:border-blue-500 hover:ring-blue-500 focus:border-green-700 focus:ring-green-700',
+      text: 'text-green-800',
+      buttons: 'bg-green-500 hover:bg-green-600 text-white',
+      },
+    dark: {
+      titlebar_bg: 'bg-slate-700',
+      bg: 'bg-slate-800',
+      bg_responsive: '',
+      component: 'bg-slate-700 hover:bg-slate-600 focus:border-blue-500 focus:ring-blue-500 hover:border-blue-500 hover:ring-blue-500',
+      text: 'text-white',
+      buttons: 'bg-slate-500 text-white',
+    },
+  };
+  
