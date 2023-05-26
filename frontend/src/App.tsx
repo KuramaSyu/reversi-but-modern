3108eb9359861d8207941fcf6d309c81b80ccdfa
KUR4M4 <52566613+zp33dy@users.noreply.github.com>
2023-05-26 13:19:16 +0200

    improved Board component and added animations

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,13 +11,13 @@ const TitleBar: React.FC<{ title: string; darkTheme: boolean; onDarkThemeToggle:
   darkTheme,
   onDarkThemeToggle,
 }) => {
-  const theme = darkTheme ? "theme-nordic" : "theme-pastel";
+  const theme = darkTheme ? "theme-nordic" : "theme-neon";
 
   return (
     <div className={`title-bar ${theme} bg-b text-highlight-c flex justify-between`}>
-      <h1 className={`px-10 mx-20 my-1 py-2 text-3xl font-light`}>{title}</h1>
+      <h1 className={`ml-3 lg:mx-24 my-1 py-2 text-3xl font-light`}>{title}</h1>
       <button
-        className={`px-10 mx-20 my-1 py-2 rounded-xl bg-d hover:bg-c
+        className={`mr-3 lg:mx-24 my-1 py-2 rounded-xl bg-d hover:bg-c
         focus:border-blue-500`}
         onClick={onDarkThemeToggle}
       >
@@ -30,31 +30,122 @@ const TitleBar: React.FC<{ title: string; darkTheme: boolean; onDarkThemeToggle:
 
 interface BoardProps {
   theme: string;
+  rows: number;
+  cols: number;
+  socket: WebSocket | null;
 }
 
-class Board extends React.Component<BoardProps> {
+interface BoardState {
+  active_row: number;
+  active_col: number;
+}
+
+class Board extends React.Component<BoardProps, BoardState> {
+  active_attrs: string;
+  unactive_attrs: string;
+  constructor(props: BoardProps) {
+    super(props);
+    this.state = {
+      active_row: -1,
+      active_col: -1,
+    };
+    this.active_attrs = "bg-b text-highlight-b font-medium text-xl";
+    this.unactive_attrs = "text-highlight-b font-extralight text-xl";
+  }
   render() {
     const squares = [];
-    const { theme } = this.props;
-    for (let row = 0; row < 8; row++) {
-      for (let col = 0; col < 8; col++) {
+    const letters = ["A", "B", "C", "D", "E", "F", "G", "H"];
+    const { active_row, active_col } = this.state;
+    const row_amount = this.props.rows;
+    const col_amount = this.props.cols;
+    const numberLabels = Array.from({ length: col_amount -1 }, (_, i) => i + 1);
+    const { theme, socket } = this.props;
+
+    // call websocket on click
+    const handleClick = (row: number, col: number) => {
+      if (socket) {
+
+        socket.send(`clicked on ${numberLabels[col]}${letters[row]}`);
+
+      }
+    };
+    // generate squares
+    for (let row = 0; row < row_amount; row++) {
+      for (let col = 0; col < col_amount; col++) {
+        // add orientation squares
+        if (row === row_amount - 1 && col === 0) {
+          // add empty square
+          squares.push(
+            <div
+              key={`${row}-${col}`}
+              className={`pb-[100%]`}
+              
+            ></div>
+          );
+          continue;
+        }
+        if (row < row_amount - 1 && col === 0) {
+          // add letters
+          // background if active
+          const active_attrs = row === active_row ? this.active_attrs : this.unactive_attrs;
+          const letter = letters[row];
+          squares.push(
+            <div
+              key={`${row}-${col}`}
+              className={`flex justify-center items-center rounded-full transition duration-700 ease-out ${active_attrs}`}
+            >
+              {letter}
+            </div>
+          );
+          continue;
+        }
+        if (row === row_amount -1 && col > 0) {
+          // add numbers
+          // background if active
+          const active_attrs = col === active_col ? this.active_attrs : this.unactive_attrs;
+          const number = numberLabels[col -1];
+          squares.push(
+            <div
+              key={`${row}-${col}`}
+              className={`flex justify-center items-center rounded-full transition duration-700 ease-out ${active_attrs}`}
+            >
+              {number}
+            </div>
+          );
+          continue;
+        }
+        // add normal squares
         const color = (row + col) % 2 === 0 ? "bg-b " : "bg-d";
         squares.push(
           <div
             key={`${row}-${col}`}
-            className={`w-[100%] pb-[100%] ${color} hover:bg-highlight-d transition duration-1000 ease-out`}
+            className={`pb-[100%] ${color} hover:bg-highlight-d transition duration-1000 ease-out`}
+            // set state of active row and col
+            onMouseEnter={() => {
+              this.setState({
+                active_row: row,
+                active_col: col,
+              });
+            }}
+            onClick={() => {
+              handleClick(row, col);
+            }}
           ></div>
         );
       }
     }
 
     return (
-      <div className={`${theme} grid grid-cols-8`}>
+
+      <div className={`${theme} grid grid-cols-9`}>
       {squares}
       </div>
     );
   }
 }
+
+
+
 
 
 
@@ -91,38 +182,42 @@ const WebSocketDemo: React.FC = () => {
     setDarkTheme(!darkTheme);
   };
 
-  const theme = darkTheme ? "theme-nordic" : "theme-pastel";
+  const theme = darkTheme ? "theme-nordic" : "theme-neon";
   // return (
   // <Board theme={theme}/>
   // )
   return (
     <div className={`app ${theme} bg-a transition duration-700 ease-out`}>
       <TitleBar title="Reversi — But Modern" darkTheme={darkTheme} onDarkThemeToggle={toggleDarkTheme} />
-      <div className='flex flex-row justify-center py-2'>
-        <input
-          type="text"
-          value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
-          className={`rounded basis-[15%] mx-[3%] py-2 bg-d`}
-          placeholder='Communicate with the websocket'
-          />
-          
-        <button
-          onClick={sendMessage}
-          className={`px-10 py-2 transition duration-300 text-highlight-a rounded-full bg-d hover:bg-highlight-d`}
-        >Send</button>
-      </div>
-
       
-      <div className="flex flex-row justify-around px-[5%] max-h-[60vh]">
-        <div className='basis-[35%]'><Board theme={theme}/></div>
+      <div className="flex flex-col md:flex-row my-10 justify-around px-[5%] overflow-y-visible">
+        <div className='basis-[48%] h-[100%]'><Board theme={theme} rows={9} cols={9} socket={socket}/></div>
         
-        <div className={`bg-d text-center ml-[3%] text-highlight-b message-list flex-auto rounded-3xl overflow-auto `}>
-          {messages.map((message, index) => (
-            <p key={index}>{message}</p>
-          ))}
+        <div>
+          <div className={`bg-d text-center ml-[3%] h-[50vh] text-highlight-b message-list flex-auto rounded-3xl overflow-auto `}>
+            {messages.map((message, index) => (
+              <p key={index}>{message}</p>
+            ))}
+
+          </div>
+          <div className='flex flex-row justify-end py-2'>
+            <input
+              type="text"
+              value={inputValue}
+              onChange={(e) => setInputValue(e.target.value)}
+              // tailwind css to make input border to highlight-b when active
+              className={`rounded-full flex-auto mx-[3%] py-2 bg-d text-center hover:border-red-600`}
+              placeholder='Communicate with the websocket'
+              />
+              
+            <button
+              onClick={sendMessage}
+              className={`px-10 py-2 basis[20%] transition duration-300 text-highlight-a rounded-full bg-d hover:bg-highlight-d`}
+            >Send</button>
+          </div>
         </div>
       </div>
+
       
     </div>
   );
