8867bdb26b045b4cc741817c3b8f0410e0838d45
KUR4M4 <52566613+zp33dy@users.noreply.github.com>
2023-05-28 22:17:11 +0200

    Update event.py

--- backend/api/event.py
+++ backend/api/event.py
@@ -34,3 +34,61 @@ class Event(ABC):
             The converted Event object
         """
         raise NotImplementedError()
+    
+class GameEvent(Event):
+    @property
+    @abstractmethod
+    def session_id(self) -> int:
+        """
+        the id of the session
+        """
+        raise NotImplementedError()
+    
+    @abstractmethod
+    def check_session(self, session_id: int) -> bool:
+        """
+        Whether or not it's a valid session id
+        """
+        raise NotImplementedError()
+    
+class ReversiEvent(GameEvent):
+    session_ids: List[int] = []
+
+    def check_session(self, session_id: int) -> bool:
+        return True
+        return session_id in self.session_ids
+
+class ChipPlacedEvent(ReversiEvent):
+    def __init__(self, user: User, session_id: int, row: int, column: int):
+        self._user = user
+        self._session_id = session_id
+        self.row = row
+        self.column = column
+    
+    @property
+    def user(self) -> User:
+        return self._user
+    
+    @property
+    def session_id(self) -> int:
+        return self._session_id
+    
+    @property
+    def row(self) -> int:
+        return self.row
+    
+    @property
+    def column(self) -> int:
+        return self.column
+    
+    def check_session(self, session_id: int) -> bool:
+        return self.session_id == session_id
+    
+    @classmethod
+    def from_json(cls, data: Dict[str, Any]) -> "ChipPlacedEvent":
+        return cls(
+            User(data["user_id"]),
+            data["session_id"],
+            data["data"]["row"],
+            data["data"]["column"]
+        )
