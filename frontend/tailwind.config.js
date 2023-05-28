0bb8799d409b971787cda5cbed2ebcce555294e7
KUR4M4 <52566613+zp33dy@users.noreply.github.com>
2023-05-28 16:50:54 +0200

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -18,30 +18,31 @@ module.exports = {
         "highlight-d": 'rgb(var(--highlight-d) / <alpha-value>)',
           ...colors
       },
-      // backgroundColor: {
-      //   "a" : "var(--color-a)",
-      //   "b" : "var(--color-b)",
-      //   "c" : "var(--color-c)",
-      //   "d" : "var(--color-d)",
-      //   "highlight-a": "var(--highlight-a)",
-      //   "highlight-b": "var(--highlight-b)",
-      //   "highlight-c": "var(--highlight-c)",
-      //   "highlight-d": "var(--highlight-d)",
-      //     ...colors
-      // },
-      // borderColor: {
-      //   "a" : "var(--color-a)",
-      //   "b" : "var(--color-b)",
-      //   "c" : "var(--color-c)",
-      //   "d" : "var(--color-d)",
-      //   "highlight-a": "var(--highlight-a)",
-      //   "highlight-b": "var(--highlight-b)",
-      //   "highlight-c": "var(--highlight-c)",
-      //   "highlight-d": "var(--highlight-d)",
-      //   ...colors
-      // },
-      extend: {},
+      extend: {
+        animation: {
+          cursor: 'cursor .6s linear infinite alternate',
+          type: 'type 1.8s ease-out .8s 1 normal both',
+          'type-reverse': 'type 1.8s ease-out 0s infinite alternate-reverse both',
+        },
+        keyframes: {
+          type: {
+            '0%': { width: '0ch' },
+            '5%, 10%': { width: '1ch' },
+            '15%, 20%': { width: '2ch' },
+            '25%, 30%': { width: '3ch' },
+            '35%, 40%': { width: '4ch' },
+            '45%, 50%': { width: '5ch' },
+            '55%, 60%': { width: '6ch' },
+            '65%, 70%': { width: '7ch' },
+            '75%, 80%': { width: '8ch' },
+            '85%, 90%': { width: '9ch' },
+            '95%': { width: '10ch' },
+          },
+        },
+      },
     },
-  plugins: [],
+  plugins: [
+    require('tailwind-scrollbar'),
+  ],
 }
 
