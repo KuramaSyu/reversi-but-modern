d5dc7dd0158d791475f0f237422099ee6f4c37fd
KUR4M4 <52566613+zp33dy@users.noreply.github.com>
2023-06-06 15:09:15 +0200

    moved session methods to session_manager

--- backend/main.py
+++ backend/main.py
@@ -1,56 +1,27 @@
 import asyncio
 from typing import *
 from datetime import datetime
+from typing import Any
 import tornado
+from tornado import httputil
 from tornado.websocket import WebSocketHandler
 from tornado.web import RequestHandler, Application
 import random
 
-websockets: Dict[int, WebSocketHandler] = {}
-sessions: Dict[str, List[int]] = {}
+from session_manager import SessionManager
+from event_handler import EventHandler
 
-def get_id() -> int:
-    code = random.randint(1000000, 9999999)
-    if code in websockets:
-        return get_id()
-    return code
 
-def add_session_ws(session: str, ws_id: int) -> None:
-    if session not in sessions:
-        sessions[session] = []
-    sessions[session].append(ws_id)
-
-def remove_session_ws(session: str, ws_id: int) -> None:
-    """
-    Removes a websocket from a session.
-    If the session is empty, it will be deleted
-    """
-    if session not in sessions:
-        return
-    sessions[session].remove(ws_id)
-    if len(sessions[session]) == 0:
-        del sessions[session]
-
-def get_session_ws(session: str) -> List[WebSocketHandler]:
-    if session not in sessions:
-        return []
-    return [websockets[ws_id] for ws_id in sessions[session]]
-
-def create_session() -> str:
-    """
-    Generates a session code which contains 4 letters
-    """
-    letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    code = "".join([random.choice(letters) for _ in range(4)])
-    if code in sessions:
-        return create_session()
-    sessions[code] = []
-    return code
 
 
 class WebSocket(WebSocketHandler):
     _id: int = -1
     _session: str|None = None
+
+    def __init__(self, *args, **kwargs: Any) -> None:
+        self.event_handler = EventHandler(self)
+        super().__init__(*args, **kwargs)
+
     def check_origin(self, origin):
         return True
 
@@ -63,23 +34,25 @@ class WebSocket(WebSocketHandler):
             "sender": "server"
         }
     def open(self):
-        self._id = get_id()
-        websockets[self._id] = self
+        self._id = SessionManager.get_id()
+        SessionManager.websockets[self._id] = self
         print(f"WebSocket with id {self._id} opened")
 
-    def on_message(self, message):
+    async def on_message(self, message):
         print(f"Message received from {self._id}: {message}")
-        
-        self.write_message(self.to_json(message, "echo", 200))
+        await self.event_handler.dispatch(message)
 
     def on_close(self):
-        del websockets[self._id]
+        del SessionManager.websockets[self._id]
+        if self._session is not None:
+            SessionManager.remove_session_ws(self._session, self._id)
+            print(f"WebSocket with id {self._id} removed from session {self._session}")
         print(f"WebSocket with id {self._id} closed")
 
 
 class CreateSessionHandler(RequestHandler):
   def get(self):
-    code = create_session()
+    code = SessionManager.create_session()
     self.write({
         "status": 200,
         "data": {
