a7580c78dc41c8f12c60b6d2d3631357430f2107
KUR4M4 <52566613+zp33dy@users.noreply.github.com>
2023-06-06 10:23:39 +0200

    improved Lobby UI

--- frontend/src/pages/Lobby.tsx
+++ frontend/src/pages/Lobby.tsx
@@ -4,6 +4,7 @@ import {ReactComponent as CopySvg} from '../svg/copy.svg';
 
 const Lobby: React.FC = () => {
   const [sessionCode, setSessionCode] = useState('');
+  const [copied, setCopied] = useState(''); // contains the copied text
 
   useEffect(() => {
     const fetchData = async () => {
@@ -23,13 +24,24 @@ const Lobby: React.FC = () => {
   return (
     <div className="flex flex-col h-full text-6xl font-serif text-highlight-a justify-around items-center text-center">
       <div className='flex flex-row'>
-        <div className='border-highlight-c border-solid border-4 py-4 px-4 rounded-3xl'>
-          <p>Lobby Code: {sessionCode}</p>
+        <div className='relative'>
+          <div className='relative border-highlight-c border-solid border-[1px] py-4 px-4 rounded-3xl'>
+            <p className=''>Lobby Code: {sessionCode}</p>
+          </div>
+          <div className='absolute w-full h-full top-0 left-0 border-highlight-c border-solid border-[12px] rounded-3xl blur-xl'>
+          </div>
         </div>
-        <div className='w-24 h-24'>
-          <CopySvg className="w-full h-full mx-10 p-3 rounded-xl bg-b text-highlight-a fill-current border-highlight-c border-solid border-4" />
-        </div>
-      </div>
+        
+        
+        <CopyToClipboard text={sessionCode}
+          onCopy={() => setCopied(sessionCode)}>
+          <div className='w-24 h-24'>
+            <CopySvg className={`w-full h-full mx-10 p-4 rounded-3xl text-highlight-a fill-highlight-a 
+            ${copied === sessionCode? "bg-d":"bg-d fill-a"} border-highlight-c border-solid border-[1px]
+            transition-all duration-300`} />
+          </div>
+        </CopyToClipboard>
+      </div>  
 
     </div>
   );
