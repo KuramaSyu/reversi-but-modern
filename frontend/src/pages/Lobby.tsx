a207f3508ebc6ed4e94c970ee5402f520af4ce93
KUR4M4 <52566613+zp33dy@users.noreply.github.com>
2023-06-13 14:57:06 +0200

    started implementing the lobby ws

--- frontend/src/pages/Lobby.tsx
+++ frontend/src/pages/Lobby.tsx
@@ -5,47 +5,53 @@ import {ReactComponent as CopySvg} from '../svg/copy.svg';
 
 const Lobby: React.FC = () => {
   const [sessionCode, setSessionCode] = useState('');
+  const [joinedSessionCode, setJoinedSessionCode] = useState(''); // contains the session code that the user has joined
   const [copied, setCopied] = useState(''); // contains the copied text
   const navigate = useNavigate(); 
   const [ws, setWs] = useState<WebSocket | null>(null);
   const [userIds, setUserIds] = useState<Array<number>>([]);
   const [playButtonClicked, setPlayButtonClicked] = useState(false);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('http://localhost:8888/create_session');
-        const data = await response.json();
-        const sessionCode = data.data.code;
-        setSessionCode(sessionCode);
-      } catch (error) {
-        console.error('Error fetching session code:', error);
-      }
-    };
-
-    fetchData();
-  }, []);
+  // generate a random 16 digit number
+  const custom_id: number = Math.floor(1000000000000000 + Math.random() * 9000000000000000);
 
     useEffect(() => {
-      // Establish WebSocket connection
-      const socket = new WebSocket('ws://localhost:8888/game');
-      setWs(socket);
 
+      // Establish WebSocket connection
+      const socket = new WebSocket('ws://localhost:8888/lobby');
+      setWs(socket);
+      // join lobby
+      socket.onopen = () => {
+        // create new session code
+        socket.send(JSON.stringify({
+          event: 'SessionCreateEvent',
+        }));
+
+
+      };
       // Handle WebSocket messages
       socket.onmessage = (event) => {
         const data = JSON.parse(event.data);
-
+        // handle session join event
         if (data.event === 'SessionJoinEvent' && data.status === 200) {
           const { session, user_id } = data;
           setUserIds((prevUserIds) => [...prevUserIds, user_id]);
-          setSessionCode(session);
+          setJoinedSessionCode(session);
         }
+        // handle session create event
+        if (data.event === 'SessionCreateEvent' && data.status === 200) {
+          setSessionCode(data.session);
+          socket.send(JSON.stringify({
+            event: 'SessionJoinEvent',
+            session: data.session,
+            custom_id: custom_id,
+          }));
       };
 
       // Cleanup WebSocket connection on component unmount
       return () => {
         socket.close();
       };
+      } 
     }, []);
 
   return (
@@ -53,14 +59,21 @@ const Lobby: React.FC = () => {
       items-center text-center gap-24">
       <div className='flex flex-row'>
         <div className='relative'>
-          <div className='relative border-highlight-c border-solid border-[1px] py-4 px-4 rounded-3xl'>
-            <p className='font-light'>Lobby Code: {sessionCode}</p>
+          <div className='relative flex border-highlight-c border-solid border-[1px] py-4 px-4 rounded-3xl'>
+            <p className='font-light'>Lobby Code: </p>
+            <input
+              type="text"
+              className='flex z-10 w-56 text-center border-none outline-none justify-center font-light bg-transparent'
+              value={joinedSessionCode}
+              onChange={(e) => setJoinedSessionCode(e.target.value)}
+            />
           </div>
           <div className='absolute w-full h-full top-0 left-0 border-highlight-c border-solid border-[12px] rounded-3xl blur-xl'>
           </div>
         </div>
-        <CopyToClipboard text={sessionCode}
-          onCopy={() => setCopied(sessionCode)}>
+
+        <CopyToClipboard text={joinedSessionCode}
+          onCopy={() => setCopied(joinedSessionCode)}>
           <div className='w-24 h-24'>
             <CopySvg className={`w-full h-full mx-10 p-4 rounded-3xl 
              border-highlight-c border-solid border-[1px]
@@ -70,7 +83,7 @@ const Lobby: React.FC = () => {
         </CopyToClipboard>
       </div>
       <div className='flex relative'
-      onClick={() => navigate(`/game/${sessionCode}`)}>
+      onClick={() => navigate(`/game/${joinedSessionCode}`)}>
           <div className='relative border-highlight-c border-solid border-[1px] py-4 px-4 
           rounded-3xl z-10 font-thin hover:text-c hover:bg-highlight-c transition duration-300 ease-in cursor-pointer'>
             PLAY
