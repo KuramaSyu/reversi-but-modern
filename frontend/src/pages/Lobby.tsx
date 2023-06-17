83971a3607f3349b195bfd4771df9458777034a7
KUR4M4 <52566613+zp33dy@users.noreply.github.com>
2023-06-17 19:08:15 +0200

    continued implementation of ws events

--- frontend/src/pages/Lobby.tsx
+++ frontend/src/pages/Lobby.tsx
@@ -3,6 +3,8 @@ import {CopyToClipboard} from 'react-copy-to-clipboard';
 import {useNavigate, useParams} from 'react-router-dom';
 import {ReactComponent as CopySvg} from '../svg/copy.svg';
 
+
+let lobby_init: boolean = false;
 const Lobby: React.FC = () => {
   const [sessionCode, setSessionCode] = useState('');
   const [joinedSessionCode, setJoinedSessionCode] = useState(''); // contains the session code that the user has joined
@@ -12,6 +14,7 @@ const Lobby: React.FC = () => {
   const [userIds, setUserIds] = useState<Array<number>>([]);
   const [playButtonClicked, setPlayButtonClicked] = useState(false);
   const { session_id } = useParams<{ session_id: string }>();
+  const [ serverMessages, setServerMessages ] = useState<Array<string>>([]);
   if (session_id !== "0" && session_id !== undefined && session_id !== sessionCode) {
     setSessionCode(session_id);
   }
@@ -19,21 +22,35 @@ const Lobby: React.FC = () => {
   const custom_id: number = Math.floor(1000000000000000 + Math.random() * 9000000000000000);
 
     useEffect(() => {
-
+      if (lobby_init === false) {
+        lobby_init = true;
+      } else {
+        return () => {
+          ws?.close();
+        }
+      }
       // Establish WebSocket connection
-      const socket = new WebSocket('ws://localhost:8888/lobby');
-      setWs(socket);
+      var socket: WebSocket; 
+      if (ws === null) {
+        socket = new WebSocket('ws://localhost:8888/lobby');
+      } else {
+        socket = ws;
+      }
+
       
       
-      if (session_id == "0") {
+      if (session_id === "0") {
         // join lobby
+        console.log("session_id is 0");
         socket.onopen = () => {
+          
           // create new session code
           socket.send(JSON.stringify({
             event: 'SessionCreateEvent',
           }));
         };
       } else {
+        console.log("session_id is not 0");
         // join lobby with session code
         socket.onopen = () => {
           socket.send(JSON.stringify(
@@ -47,16 +64,23 @@ const Lobby: React.FC = () => {
       }
       // Handle WebSocket messages
       socket.onmessage = (event) => {
+        // log time in hh:mm:ss format
+        var time = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', second: '2-digit' });
+        setServerMessages((prevServerMessages) => [`${time}: ${event.data}`, ...prevServerMessages]);
         const data = JSON.parse(event.data);
         // handle session join event
         if (data.event === 'SessionJoinEvent' && data.status === 200) {
-          const { session, user_id } = data;
-          setUserIds((prevUserIds) => [...prevUserIds, user_id]);
+          console.log("SessionJoinEvent");
+          const { session } = data;
+          const user_id = data.data.user_id;
+          setUserIds((prevUserIds) => data.data.all_users);
           setJoinedSessionCode(session);
         }
         // handle session create event
         if (data.event === 'SessionCreateEvent' && data.status === 200) {
           // setSessionCode(data.session);
+          console.log("SessionCreateEvent");
+          setUserIds([]);
           navigate(`/lobby/${data.session}`);
           socket.send(JSON.stringify({
             event: 'SessionJoinEvent',
@@ -65,7 +89,8 @@ const Lobby: React.FC = () => {
           }));
         }
         // handle game start event
-        if (data.event === 'GameStartEvent' && data.status === 200 && data.session_id === joinedSessionCode) {
+        if (data.event === 'GameStartEvent' && data.status === 200 && data.session === joinedSessionCode) {
+          console.log("GameStartEvent");
           navigate(`/game/${data.session}`);
         }
 
@@ -77,8 +102,8 @@ const Lobby: React.FC = () => {
     }, []);
 
   return (
-    <div className="flex flex-col h-full text-6xl text-highlight-a justify-center 
-      items-center text-center gap-24">
+    <div className="flex flex-col text-6xl text-highlight-a justify-center 
+      items-center text-center gap-10 py-10 scrollbar-thin scrollbar-thumb-d scrollbar-track-b overflow-auto">
       <div className='flex flex-row'>
         <div className='relative'>
           <div className='relative flex border-highlight-c border-solid border-[1px] py-4 px-4 rounded-3xl'>
@@ -119,6 +144,30 @@ const Lobby: React.FC = () => {
           </div>
           <div className='absolute w-full h-full top-0 left-0 border-highlight-c border-solid border-[12px] rounded-3xl blur-xl'>
           </div>
+      </div>
+      <div className='flex flex-row gap-6'>
+        {}
+        {userIds.map((userId) => {
+          return (
+            <div key={userId} className='flex flex-col items-center justify-center'>
+              <div className='relative w-32 h-32'>
+                <div className='absolute w-full h-full top-0 left-0 border-highlight-c border-solid border-[12px] rounded-full blur-xl'>
+                  
+                </div>
+                <div className='relative w-full h-full border-highlight-c border-solid border-[1px] rounded-full'>
+                  <p>{userId ? `${userId.toString().slice(0, 2)}\n${userId.toString().slice(2)}`:'0'}</p>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <div className='rounded-3xl bg-b mx-10 font-mono text-[10px] px-3 py-3 text-left'>
+        {serverMessages.map((message) => {
+          return (
+            <p>{message}</p>
+          );
+        })}
       </div>
 
     </div>
