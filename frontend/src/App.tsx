9f0b8b0184a69e665eaeb4c96953163f154a50fe
KUR4M4 <52566613+zp33dy@users.noreply.github.com>
2023-06-28 15:36:46 +0200

    cleaned up code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,14 +11,14 @@ interface TitleBarProps {
 }
 
 const default_animation = 'transition duration-1000 ease-out';
-/// TitleBar component
+// TitleBar component
 const TitleBar: React.FC<TitleBarProps> = ({ title, theme, themes, onCycleTheme }) => {
   const navigate = useNavigate();
   return (
     <div className={`relative title-bar bg-b text-highlight-c flex justify-between ${default_animation}`}>
       <div className={`ml-3 lg:mx-24 my-2 z-10 text-5xl font-thin hover:text-highlight-a 
       cursor-pointer transition duration-300`}
-      onClick={() => navigate("/lobby/0")}>{title}</div>
+        onClick={() => navigate("/lobby/0")}>{title}</div>
       {/* <div className={`absolute top-0 left-0 ml-3 lg:mx-24 my-2 text-highlight-c text-5xl font-normal 
       blur-md`}>{title}</div> */}
       <button
@@ -31,7 +31,7 @@ const TitleBar: React.FC<TitleBarProps> = ({ title, theme, themes, onCycleTheme 
   );
 };
 
-/// App component
+// App component
 const App: React.FC = () => {
   const [theme, setTheme] = useState('theme-nordic');
 
@@ -52,7 +52,7 @@ const App: React.FC = () => {
       <Router>
         <TitleBar title="Reversi" theme={theme} themes={themes} onCycleTheme={cycleTheme} />
         <Routes>
-          <Route path='/' element={<Navigate to='/lobby' />} />
+          <Route path='/' element={<Navigate to='/lobby/0' />} />
           <Route path="/game/:session_id" element={<Reversi theme={theme} />} />
           <Route path="/lobby/:session_id" element={<Lobby />} />
         </Routes>
