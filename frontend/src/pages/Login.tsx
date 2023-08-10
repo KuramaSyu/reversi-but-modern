fc8fe7a5c8ab72e349796de0cf5e770dc2f9322a
kurama <52566613+zp33dy@users.noreply.github.com>
2023-08-10 13:37:55 +0200

    Create Login.tsx

---
+++ frontend/src/pages/Login.tsx
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import config from "../app.config.json";
+
+const backendName = config.backend.name;
+
+interface Credentials {
+    username: string;
+    password: string;
+  }
+  
+  async function loginUser(credentials: Credentials): Promise<string> {
+    return fetch(`http://${backendName}:8888/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(credentials)
+    })
+      .then(data => data.json())
+      .then(response => response.token as string); // Assuming your API response has a "token" field
+  }
+  
+
+
+interface LoginProps {
+  setToken: (token: string) => void;
+}
+
+const Login: React.FC<LoginProps> = ({ setToken }) => {
+  const [username, setUserName] = useState<string | undefined>();
+  const [password, setPassword] = useState<string | undefined>();
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    if (username && password) {
+      const token = await loginUser({
+        username,
+        password
+      });
+      setToken(token);
+    }
+  };
+
+  return (
+    <div className="login-wrapper">
+      <h1>Please Log In</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <p>Username</p>
+          <input type="text" onChange={(e) => setUserName(e.target.value)} />
+        </label>
+        <label>
+          <p>Password</p>
+          <input type="password" onChange={(e) => setPassword(e.target.value)} />
+        </label>
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+Login.propTypes = {
+  setToken: PropTypes.func.isRequired
+};
+
+export default Login;
