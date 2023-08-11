11beed5d6952850f71e86c86389061f2a2e6b942
kurama <52566613+zp33dy@users.noreply.github.com>
2023-08-11 11:38:21 +0200

    Update script.sql

--- backend/core/script.sql
+++ backend/core/script.sql
@@ -1,20 +1,32 @@
-CREATE SCHEMA IF NOT EXISTS user;
-CREATE TABLE IF NOT EXISTS user.information {
+CREATE SCHEMA IF NOT EXISTS profile;
+CREATE TABLE IF NOT EXISTS profile.information (
     id SERIAL PRIMARY KEY,
     username VARCHAR(255) UNIQUE NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT NOW()
-};
+);
 
-CREATE TABLE IF NOT EXISTS user.authentication {
+CREATE TABLE IF NOT EXISTS profile.authentication (
     id SERIAL PRIMARY KEY,
-    user_id INTEGER REFERENCES user.information(id) ON DELETE CASCADE,
+    user_id INTEGER REFERENCES profile.information(id) ON DELETE CASCADE,
     password_hash VARCHAR(512) NOT NULL,
     salt VARCHAR(255) NOT NULL
-};
+);
 
-CREATE TABLE IF NOT EXISTS user.profile_picture {
+CREATE TABLE IF NOT EXISTS profile.picture (
     id SERIAL PRIMARY KEY,
-    user_id INTEGER REFERENCES user.information(id) ON DELETE CASCADE,
+    user_id INTEGER REFERENCES profile.information(id) ON DELETE CASCADE,
     picture BYTEA NOT NULL,
     thumbnail BYTEA NOT NULL
-};
+);
+CREATE OR REPLACE VIEW profile.full AS
+SELECT
+    i.id AS information_id,
+    i.username,
+    i.created_at,
+    a.id AS authentication_id,
+    a.password_hash,
+    a.salt
+FROM
+    profile.information i
+LEFT JOIN
+    profile.authentication a ON i.id = a.user_id;
