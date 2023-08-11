decf5b4f8ce8ec431241d55328c05bc5dba94051
kurama <52566613+zp33dy@users.noreply.github.com>
2023-08-11 11:39:01 +0200

    Update login.py

--- backend/handlers/login.py
+++ backend/handlers/login.py
@@ -5,6 +5,9 @@ from tornado import httputil
 from tornado.websocket import WebSocketHandler
 from tornado.web import RequestHandler, Application
 
+from core import Table
+
+
 class LoginHandler(RequestHandler):
     def __init__(self, *args, **kwargs: Any) -> None:
         super().__init__(*args, **kwargs)
@@ -15,10 +18,30 @@ class LoginHandler(RequestHandler):
         self.set_header('Access-Control-Allow-Methods', 'POST, OPTIONS')
         self.set_header('Access-Control-Allow-Headers', 'Content-Type')
 
-    async def post(self, username: str, password: str):
+    async def post(self):
+        password = self.get_argument("password")
+        username = self.get_argument("username")
+        table = Table("profile.full")
+        record = await table.select_row(where={"username": username})
+        if not record:
+            return self.write(
+                {
+                    "status": 400,
+                    "message": "Username or password is incorrect"
+                }
+            )
+        is_pw_correct = bcrypt.checkpw(password.encode(), record["password_hash"].encode())
+        if not is_pw_correct:
+            return self.write(
+                {
+                    "status": 400,
+                    "message": "Wrong password"
+                }
+            )
         self.write(
             {
                 "status": 200,
+                "message": "Login successful",
                 "token": "test"
             }
         )
