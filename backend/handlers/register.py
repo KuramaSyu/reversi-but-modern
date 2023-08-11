5ead7375d8a4cadde0668b8919e1d6ea5be81479
kurama <52566613+zp33dy@users.noreply.github.com>
2023-08-11 11:39:10 +0200

    Update register.py

--- backend/handlers/register.py
+++ backend/handlers/register.py
@@ -5,6 +5,8 @@ from tornado import httputil
 from tornado.websocket import WebSocketHandler
 from tornado.web import RequestHandler, Application
 
+from core import Table
+
 class SignInHandler(RequestHandler):
     def __init__(self, *args, **kwargs: Any) -> None:
         super().__init__(*args, **kwargs)
@@ -18,8 +20,33 @@ class SignInHandler(RequestHandler):
     async def post(self):
         password = self.get_argument("password")
         username = self.get_argument("username")
+        table = Table("profile.full")
+        record = await table.select_row(where={"username": username})
+        if record:
+            return self.write(
+                {
+                    "status": 400,
+                    "message": "Username already exists",
+                    "username": record["username"],
+                    "password_hash": record["password_hash"],
+                    "salt": record["salt"]
+
+                }
+            )
+
         salt: bytes = bcrypt.gensalt()
         hashed_password: bytes = bcrypt.hashpw(password.encode(), salt)
+        table = Table("profile.information")
+        record = await table.insert(values={"username": username})
+        user_id = record[0]["id"]
+        table = Table("profile.authentication")
+        record = await table.insert(
+            values={
+                "user_id": user_id, 
+                "salt": salt.decode(), 
+                "password_hash": hashed_password.decode()
+            }
+        )
         self.write(
             {
                 "status": 200,
@@ -27,5 +54,7 @@ class SignInHandler(RequestHandler):
                 "salt": salt.decode(),
                 "hashed_password": hashed_password.decode(),
                 "password": password,
+                "username": username,
+                "user_id": user_id
             }
         )
